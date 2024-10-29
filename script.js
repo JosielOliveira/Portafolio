d03de35cf36f31bc16852036147f763e80ed0e0b
JosielOliveira <joliviera.prof@gmail.com>
2024-10-29 21:25:15 +0100

    tema oscuro

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector("form").addEventListener("submit", function(e) {
+    e.preventDefault(); // Evita que se recargue la página
+    alert("Gracias por contactarme, te responderé pronto.");
+});
